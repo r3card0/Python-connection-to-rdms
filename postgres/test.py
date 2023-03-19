20b36f9d6e0fd30570512b982a74f3a24decb922
Ricardo Saldívar <95096808+r3card0@users.noreply.github.com>
2023-03-19 15:12:03 -0600

    example of how to connect

---
+++ postgres/test.py
@@ -0,0 +1,26 @@
+from connect_postgres import Connect
+
+# global variables to connect
+conn = Connect()
+connection = conn.connect()
+
+# sql query
+def query():
+    sql = """
+    SELECT * FROM table1
+    limit 5
+    """
+    return sql
+
+# dataframe creation
+def dataframe():
+    df = pd.read_sql_query(query(),connection)
+    df = df.copy(deep=True)
+    return df
+
+def run():
+    print(dataframe())
+
+
+if __name__ == "__main__":
+    run()
