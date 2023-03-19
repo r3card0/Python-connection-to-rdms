49a0c20536e8dc77ab39059870c256e436bc2185
Ricardo Saldívar <95096808+r3card0@users.noreply.github.com>
2023-03-19 15:01:05 -0600

    config for postgres

---
+++ postgres/config.py
@@ -0,0 +1,18 @@
+from configparser import ConfigParser
+
+class Config:
+    def __init__(self) -> None:
+        self.parser = ConfigParser()
+
+    def config(self,filename='/path/database.ini',section = 'postgres'):
+        db = {}
+        self.parser.read(filename)
+
+        if self.parser.has_section(section):
+            self.params = self.parser.items(section)
+            for param in self.params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section,filename))
+        
+        return db
