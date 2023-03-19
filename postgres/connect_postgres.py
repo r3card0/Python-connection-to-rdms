add8d93b4682ccbe7e1a843d6a9b6afac6fd6aa7
Ricardo Saldívar <95096808+r3card0@users.noreply.github.com>
2023-03-19 15:15:26 -0600

    postgres connection file

---
+++ postgres/connect_postgres.py
@@ -0,0 +1,20 @@
+
+
+import psycopg2 as p2
+from config import Config
+
+# global variables
+x = Config()
+
+class Connect:
+    def __init__(self):
+        self.params = x.config()
+
+    def connect(self):
+        self.connection = p2.connect(**self.params)
+        return self.connection
+
+
+if __name__ == "__main__":
+    connection = Connect()
+    connection.connect()
